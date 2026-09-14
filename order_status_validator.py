6b2387a4b7420184464c6a2cc5dc380589bd8466
Yaroslav Martsul <yaroslav.marcul105@gmail.com>
2026-09-14 23:06:33 +0300

    Create order_status_validator.py

---
+++ order_status_validator.py
@@ -0,0 +1,9 @@
+order_info = {
+    "title": "Парсер для сайта",
+    "budget": 400,
+    "is_finished": True 
+    }
+if order_info["is_finished"] == True:
+    print("Заказ выполнен! Деньги отправлены.")
+else:
+    print("Заказ ещё в работе.")
