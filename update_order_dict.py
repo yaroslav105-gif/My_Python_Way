6e0937f162e5f8ccadf7a954387e5892dbc967c1
Yaroslav Martsul <yaroslav.marcul105@gmail.com>
2026-09-14 22:58:48 +0300

    Create update_order_dict.py

---
+++ update_order_dict.py
@@ -0,0 +1,9 @@
+order_info = {
+    "title": "Парсер для сайта",
+    "budget": 300,
+    "is_finished": False
+    }
+
+order_info["budget"] = 400
+order_info["is_finished"] = True
+print(order_info)
